451dc536dd8f3584ccbbab2dcf5aa02881a5f176
Júlia Birkett <juliambirkett@gmail.com>
2016-10-08 15:51:31 -0300

    Usa somente uma classe para realizar conexão

--- src/br/com/fiap/conexao/ConexaoFactory.java
+++ src/br/com/fiap/conexao/ConexaoFactory.java
@@ -4,6 +4,16 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConexaoFactory {
+	private static Connection conexao;
+	
+	public static Connection getConexao() throws Exception {
+		if (conexao == null) {
+			conexao = new ConexaoFactory().getConnection();
+		}
+		
+		return conexao;
+	}
+	
 	public Connection getConnection() throws Exception {
 		String url = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
 		String DBUSER = "OPS$RM76507";
